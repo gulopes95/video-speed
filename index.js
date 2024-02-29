35861080c9ec71b31e010a3bdd61312b698c7747
Gustavo Lopes <gu.lopes@hotmail.com>
2024-02-29 18:19:30 -0300

    Commands/Shortcuts

--- index.js
+++ index.js
@@ -17,3 +17,28 @@ function changeVideoSpeed(speed) {
 document.querySelectorAll('.btn-speed-option').forEach(button => {
   button.addEventListener('click', function() { executeChromeScripts(button.id) });
 });
+
+let buttons = document.querySelectorAll('.btn-speed-option');
+
+for (let i = 0; i < 10; i++) {
+  document.addEventListener('keyup', function(event) {
+    if (event.key == i.toString() && i > -1 && i < 10) {
+      buttons[i+2].click();
+    }
+  });
+}
+
+/*
+0  0.25
+1  0.50
+2  0.75
+3  1.00
+4  1.25
+5  1.50
+6  1.75
+7  2.00
+8  2.25
+9  2.50
+10 2.75
+11 3.00
+*/
